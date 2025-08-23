9e80ec4fd53d9aedb369e9c88fff2459ad5639e6
Hudayub1 <hudalayubi@gmail.com>
2025-08-23 18:12:15 +0800

    Finished MVP for this project
    added domcontentLoaded and clearContent function

--- src/index.js
+++ src/index.js
@@ -4,21 +4,38 @@ import {renderAbout} from './tabs/about'
 import {renderHome} from './tabs/home'
 import {renderPrograms} from './tabs/program'
 
-const btnHome = document.querySelector('.home-js')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnHome = document.querySelector('.home-js')
 const btnAbout = document.querySelector('.about-js')
 const btnPrograms = document.querySelector('.programs-js')
 
+
+// clear content
+
+function clearContent(){
+    const div = document.getElementById('content')
+    div.innerHTML = ''
+}
+
+// button events
 btnHome.addEventListener('click', () => {
-    
+    clearContent()
     renderHome()
 })
 
 btnAbout.addEventListener('click', () => {
+    clearContent()
     renderAbout()
 })
 
 btnPrograms.addEventListener('click', () => {
+    clearContent()
     renderPrograms()
 })
 
+renderHome()
 console.log('Well Hello There Fellow Berserkers')
+
+})
+
